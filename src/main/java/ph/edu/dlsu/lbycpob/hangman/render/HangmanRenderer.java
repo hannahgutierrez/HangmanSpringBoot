08b4c1c3b4f8691174ca36c7d0226dfb3ab96efb
Hannah Gutierrez <hannah_gutierrez@dlsu.edu.ph>
2026-07-16 20:14:57 +0800

    Added a method with comments in HangmanRenderer

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
@@ -4,4 +4,10 @@ import java.io.IOException;
 import java.util.List;
 
 public interface HangmanRenderer {
+    /**
+     * Returns the lines of hangman art for {@code guessesRemaining} (0–8).
+     *
+     * @throws IOException if the art resource cannot be located or read
+     */
+    List<String> render(int guessesRemaining) throws IOException;
 }
